132fdb410e15f44a01e2341cea3f495b05d36fd6
Anwar Hossain Zahid <mdahzahid@gmail.com>
2024-06-04 15:29:27 -0700

    changing cfg for testing in the cluster.

--- varity/cfg.py
+++ varity/cfg.py
@@ -19,18 +19,19 @@ MATH_FUNC_PROBABILITY = 0.05
 NUM_GROUPS = 2
 
 # Number of tests per group
-TESTS_PER_GROUP = 10
+TESTS_PER_GROUP = 5
 
 # Set of commpilers to test.
 # COMPILERS is a list containing tuples (x, y), 
 # where x is a string with the compiler name, and y is the path to the compiler
 #COMPILERS = [("clang_80", "/usr/tce/packages/clang/clang-upstream-2019.03.26/bin/clang"), ("gcc_721", "/usr/tce/packages/gcc/gcc-7.2.1-redhat/bin/gcc"), ("xlc", "/usr/tce/packages/xl/xl-2019.02.07/bin/xlc"), ("nvcc_92", "/usr/tce/packages/cuda/cuda-9.2.148/bin/nvcc")]
-COMPILERS = [("clang_7", "/Users/lagunaperalt1/projects/GPU_work/latest_llvm/llvm-7.0/install/bin/clang"), ("gcc_7", "/opt/local/bin/gcc-mp-7")]
+# COMPILERS = [("clang_7", "/Users/lagunaperalt1/projects/GPU_work/latest_llvm/llvm-7.0/install/bin/clang"), ("gcc_7", "/opt/local/bin/gcc-mp-7")]
+COMPILERS = [("my_clang", ""), ("my_gcc", "")]
 
 # Levels of optimization to try
-OPT_LEVELS = [("-O0", 1), ("-O0", 0), ("-O1", 0), ("-O2", 0), ("-O3", 0)]
+# OPT_LEVELS = [("-O0", 1), ("-O0", 0), ("-O1", 0), ("-O2", 0), ("-O3", 0)]
 #OPT_LEVELS = ["-O0", "-O1"]
-
+OPT_LEVELS = [("-O0", 1), ("-O1", 0)]
 # Name of root directory 
 TESTS_DIR = "_tests"
 
@@ -39,7 +40,7 @@ TESTS_DIR = "_tests"
 ###############################################################################
 
 # Number of random inputs per run
-INPUT_SAMPLES_PER_RUN = 5
+INPUT_SAMPLES_PER_RUN = 2
 
 ###############################################################################
 # Flaoting-point types
